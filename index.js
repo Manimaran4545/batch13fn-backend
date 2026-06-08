8f85bc6d0136ad432067676d6bfdbe3f79fdd782
manimaran <manimaranmp45@gamil.com>
2026-06-08 11:08:17 +0530

    Added dotenv package

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const user = require("./Routes/User");
+
 const app = express();
-const port = 5000;
-const mongoose = require('mongoose');
-const user = require('./Routes/User');
-app.use(express.json());
-const task = require('./Routes/Task');
-const cors = require('cors');
+const port = process.env.PORT || 5000;
+
 app.use(cors());
-
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send('Hello World!');
+    res.send("Hello World!");
 });
-app.listen(port,() => {
-    console.log(`Server is running on port ${port}`);
-} );
 
-// User Route
-// app.use("/user", userRoute);
-
-mongoose.connect("mongodb://manimaran:manimara@ac-3bfedgn-shard-00-00.ottu39z.mongodb.net:27017,ac-3bfedgn-shard-00-01.ottu39z.mongodb.net:27017,ac-3bfedgn-shard-00-02.ottu39z.mongodb.net:27017/?ssl=true&replicaSet=atlas-12vpfw-shard-0&authSource=admin&appName=batch13an")
+mongoose.connect(process.env.MONGO_URL)
 .then(() => {
     console.log("Connected to database");
+
+    app.use("/user", user);
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
 })
 .catch((err) => {
     console.error("Error connecting to database", err);
 });
-// Server
-// app.listen(port, () => {
-//     console.log(`Server running on port ${port}`);
-// });
-
-app.use("/user", user);
